3a5e3d492c92fb1584587847f4ab489345a0ac7f
Eamonn O'Brien-Strain <e@obrain.com>
2025-01-27 01:50:37 +0000

    Fetch text

--- wikipedia.js
+++ wikipedia.js
@@ -6,12 +6,28 @@ const params = {
     rnlimit: 2
 };
 
-const url = lang => `https://${lang}.wikipedia.org/w/api.php?origin=*&` +
+
+function deWikify(text) {
+    return text;
+    //return text.replace(/\[\[(.*?)\]\]/g, (match, p1) => {
+    //    const parts = p1.split("|");
+    //    return parts[parts.length - 1];
+    //}
+}
+const randomUrl = lang => `https://${lang}.wikipedia.org/w/api.php?origin=*&` +
     Object.keys(params).map((key) => key + "=" + params[key]).join("&");
 
-    export async function getRandomPage(lang) {
-    const response = await fetch(url(lang));
-    const json = await response.json();
-    const meta = json.query.random[0];
-    return JSON.stringify(meta);
+
+export async function getRandomPage(lang) {
+    const randomJson = await (await fetch(randomUrl(lang))).json();
+    const title = randomJson.query.random[0].title.replace(/ /g, "_");
+
+    //const textUrl = `https://${lang}.wikipedia.org/w/index.php?origin=*&title=${title}&action=raw`
+
+    const textUrl = `https://${lang}.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts&titles=${title}&formatversion=2&exsentences=10&exlimit=1&exintro=1&explaintext=1&exsectionformat=plain`
+    console.log(textUrl)
+    //const text = await (await fetch(textUrl)).text();
+    //return deWikify(text);
+    const textJson = await (await fetch(textUrl)).json();
+    return textJson.query.pages[0].extract;
 }
