50e1b7b840ae4b801d6c67e16f3cd01b94b9e703
Eamonn O'Brien-Strain <e@obrain.com>
2025-01-27 03:37:23 +0000

    Tweak style

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { getRandomPage } from "./wikipedia.js";
 
-/* Global $prelude $articleTitle */
+/* Global $prelude $articleTitle $word */
 
 (async () => {
-    const {title, text} = await getRandomPage('en');
+    const { title, text } = await getRandomPage('en');
     $articleTitle.innerText = title;
     $prelude.innerText = text;
+    $word.style.width = '5em';
 })()
 
