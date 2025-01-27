785cc11b5a1f1f1ad03f0e8123d957f1c2873f19
Eamonn O'Brien-Strain <e@obrain.com>
2025-01-27 04:50:55 +0000

    First working version

--- index.js
+++ index.js
@@ -3,17 +3,40 @@ import { Text } from "./text.js";
 
 /* Global $prelude $articleTitle $word */
 
+
 (async () => {
     const { title, text } = await getRandomPage('en');
     $articleTitle.innerText = title;
     const textObj = new Text(text);
-    const { done, value } = textObj.next();
-    if (done) {
-        return;
+    let theWord = undefined
+
+    function advance() {
+        const { done, value } = textObj.next();
+        if (done) {
+            return;
+        }
+        const { prelude, word } = value
+        theWord = word
+        $prelude.innerText = prelude;
+        $word.style.width = `${word.length * 0.7}em`;
     }
-    const { prelude, word } = value
-    $prelude.innerText = prelude;
-    $word.value = word;
-    $word.style.width = `${word.length * 0.7}em`;
+    advance()
+
+    $word.addEventListener('input', () => {
+        $word.value = $word.value.toLowerCase();
+        if ($word.value.length >= theWord.length) {
+            if ($word.value === theWord) {
+                console.log('Correct!');
+            } else {
+                console.log('Wrong!');
+            }
+            $word.value = '';
+            advance();
+        } else if (!theWord.startsWith($word.value)) {
+            console.log('Wrong!');
+            $word.value = '';
+            advance();
+        }
+    })
 })()
 
